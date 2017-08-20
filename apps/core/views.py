5a706c07d7aacc455fb8fa767bb858b7a8d20a33
Truong Tuan Quang <tuanquanghpvn@gmail.com>
2017-08-20 13:35:03 +0700

    Init Project

---
+++ apps/core/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.views.generic.base import ContextMixin
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.gzip import gzip_page
+
+from apps.categories.models import Category
+from apps.posts.models import Post
+
+class BaseView(ContextMixin):
+    @method_decorator(gzip_page)
+    def dispatch(self, request, *args, **kwargs):
+        return super(BaseView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = Category.objects.all()
+        context['most_popular'] = Post.objects.order_by('-dateCreate')\
+                                        .exclude(externalView__lt=20000)[0:6]
+        context['rand'] = Post.objects.order_by('-dateCreate').order_by('?')[0:6]
+        return context
+    
+class AdminRequiredMixin(object):
+    @method_decorator(permission_required('is_superuser', login_url='/admin/login'))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+class LoginRequiredMixin(object):
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
---
+++ apps/posts/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+from apps.users.models import UserProfile
+from apps.categories.models import Category
+
+class Post(models.Model):
+    id = models.CharField(max_length=255, primary_key=True)
+    name = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255)
+    description = models.TextField(blank=True, default='')
+    videoDurationFe = models.CharField(max_length=255)
+    externalView = models.IntegerField(default=0, blank=True)
+    dateCreate = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category)
+    profile = models.ForeignKey(UserProfile, default=1)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+        db_table = 'post'
